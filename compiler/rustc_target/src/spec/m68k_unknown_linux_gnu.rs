dd28313b6922526cfef43ff092977e34ff827e81
Bobby Powers <bobbypowers@gmail.com>
2021-05-30 07:55:20 -0700

    fix for internal API changes

--- compiler/rustc_target/src/spec/m68k_unknown_linux_gnu.rs
+++ compiler/rustc_target/src/spec/m68k_unknown_linux_gnu.rs
@@ -1,20 +1,16 @@
-use crate::spec::{LinkerFlavor, Target, TargetResult};
+use crate::abi::Endian;
+use crate::spec::Target;
 
-pub fn target() -> TargetResult {
+pub fn target() -> Target {
     let mut base = super::linux_base::opts();
     base.max_atomic_width = Some(32);
+    base.endian = Endian::Big;
 
-    Ok(Target {
+    Target {
         llvm_target: "m68k-unknown-linux-gnu".to_string(),
-        target_endian: "big".to_string(),
-        target_pointer_width: "32".to_string(),
-        target_c_int_width: "32".to_string(),
+        pointer_width: 32,
         data_layout: "E-m:e-p:32:32-i8:8:8-i16:16:16-i32:32:32-n8:16:32-a:0:32-S16".to_string(),
         arch: "m68k".to_string(),
-        target_os: "linux".to_string(),
-        target_env: "gnu".to_string(),
-        target_vendor: "unknown".to_string(),
-        linker_flavor: LinkerFlavor::Gcc,
         options: base,
-    })
+    }
 }
